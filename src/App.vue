2cae2680fa54d0387968cef35d852417fbfe0168
Luis Cortez <danielcorteznew8@gmail.com>
2021-12-04 14:32:39 -0400

    Init vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="text-center">
+      <img width="100" alt="Vue logo" src="./assets/logo.png">
+    </div>
+    <formQuestion />
+  </div>
+</template>
+
+<script>
+import formQuestion from './components/formQuestion.vue'
+
+export default {
+  name: 'App',
+  components: {
+    formQuestion
+  }
+}
+</script>
+
+<style>
+.text-center{
+  text-align: center;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/formQuestion.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h1>Question</h1>
+
+    <!-- form -->
+    <form class="row g-3">
+      <div class="col-md-6">
+        <label class="form-label">Name</label>
+        <b-form-input v-model="name" placeholder="Enter your name"></b-form-input>
+      </div>
+      <div class="col-md-4">
+        <label class="form-label">Select</label>
+        <b-form-select v-model="selected" :options="options"></b-form-select>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'formQuestion',
+  data(){
+    return{
+      name: '',
+      selected: null,
+      options: [1,2,3,4,5,6,7,8]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
